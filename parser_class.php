6aa5a55a46817990659324f1f24d6b91a08a8250
Martins Pilsetnieks <pilsetnieks@gmail.com>
2011-05-11 09:48:28 +0300

    Reworked parser class

--- parser_class.php
+++ parser_class.php
@@ -1,327 +1,326 @@
-
 <?php
+   /**
+	* vCard parser class, reads a vCard file and makes each of the values accessible as object members.
+	*
+	* @link https://github.com/nuovo/vCard-parser
+	* @author Roberts Bruveris, Martins Pilsetnieks
+	* @see RFC 2426, RFC 2425
+	* @version 0.2
+	*/
+	class vCard implements Countable, Iterator
+	{
+		const MODE_ERROR = 'error';
+		const MODE_SINGLE = 'single';
+		const MODE_MULTIPLE = 'multiple';
 
-//$path = 'contacts-sales-lv-people.vcf';     
-$path='test.vcf';
-$parse = new vCard($path);
-echo '<pre>';
-print_r($parse->vCards);
+		/**
+		 * @var string Current object mode - error, single or multiple (for a single vCard within a file and multiple combined vCards)
+		 */
+		private $Mode;  //single, multiple, error
 
+		private $Path = '';
+		private $RawData = '';
 
-echo '</pre>';
+		/**
+		 * @var array Internal data container. Contains vCard objects for multiple vCards and just the data for single vCards.
+		 */
+		private $Data = array();
 
- /**
-    * 
-    * Class parses vCard file and returns array with information form vCard.
-    * 
-    * @access public
-    * 
-    * @return array with vCards
-    * 
-    */
-class vCard{
+		/**
+		 * @static Parts of structured elements according to the spec.
+		 */
+		private static $Spec_StructuredElements = array(
+			'n' => array('LastName', 'FirstName', 'AdditionalNames', 'Prefixes', 'Suffixes'),
+			'adr' => array('POBox', 'ExtendedAddress', 'StreetAddress', 'Locality', 'Region', 'PostalCode', 'Country'),
+			'geo' => array('Latitude', 'Longitude'),
+			'org' => array('Name', 'Unit1', 'Unit2')
+		);
+		private static $Spec_MultipleValueElements = array('nickname', 'categories');
 
- public $mode;  //single, multiple, error
- public $vCards = array();
- public $marker0;
- /**
-    * 
-    * Functions which reads text line by line from file
-    * and puts it into formatted array
-    *
-    * @access public
-    * 
-    * @return array with one or multiple vCards()
-    * 
-    */
- public function __construct($path){
-  $lines = file($path);
-  $card = array();
-  $remember='';
-  foreach($lines as $line){
-
-		$pos = strpos($line,':');
-		$end=strlen(trim($line));
-		if(substr(trim($line),$end-1, $end)=='='){
-			$remember=$line;
-			continue;
-		}else{
-			$remember='';
-		}
-		if($remember=''){
-		
-		//if it is beginning of card - skip  	
-		$pos=strpos($line, 'BEGIN:VCARD');
-		if($pos !== false){
-		$vCard = array();
-		continue;
-		}
-   
-	   }else{
-	   
-	   $line=$line.$remember;
-	   $remember='';
-	   
-	   //if it is end of card - skip
-	   if($pos !== false){
-		    $pos2 = strpos($line,'END:VCARD');
-			   if($pos2 !== false){
-				    $this->vCards[] = $vCard;
-				    $vCard = array();
-				    continue;
-			}
-			
-			//clear marker and line 
-	
-		    $marker=$this->escape(substr($line,0,$pos));
-		    $this->marker0 = $marker;  
-		    $line_end=$this->escape(substr($line,$pos+1,strlen($line)));
-		    $pos3=strpos($marker,';');
-	
-		    
-			//if there are arguments like N;Type=Work: John Smith
-		    if(strpos($marker,';')!==false){   
-			    $newvar=$this->split($marker,$line_end);	
-				$this->insert($newvar,$marker,$pos3,$vCard);	
-	  
-			}
-			else// if there are no arguments like FN:John Smith
+		/**
+		 * vCard constructor
+		 *
+		 * @param string Path to file, optional.
+		 * @param string Raw data, optional.
+		 *
+		 * One of these parameters must be provided, otherwise an exception is thrown.
+		 */
+		public function __construct($Path = false, $RawData = false)
+		{
+			// Checking preconditions for the parser.
+			// If path is given, the file should be accessible.
+			// If raw data is given, it is taken as it is.
+			// In both cases the real content is put in $this -> RawData
+			if ($Path)
 			{
-				$newvar=$this->split($marker,$line_end);	
-				$this->insert($newvar,$marker,strlen($marker),$vCard);			
-		
-			}
-		}
-	}
-  } 
- }
- 
- 
- /**
-    * 
-    * Functions which inserts data into array
-    * 
-    *
-    * @access public
-    * 
-    * @return nothing
-    * 
-    */
-
-public function insert($newvar,$marker,$pos3,&$vCard){
-
-  if(is_array($newvar)){
-			    while (list($key, $value) = each($newvar)) { 
-				
-				   if(!isset($vCard[substr($marker,0,$pos3)])){
-				   		$vCard[substr($marker,0,$pos3)][$key]=$value;
-
-				   }
-				   
-				   else{
-
-				   		if(is_array($vCard[substr($marker,0,$pos3)]))//if already tehre are such kid info
-				   		{				   		
-							if(isset($vCard[substr($marker,0,$pos3)][$key])){
-								if(count($vCard[substr($marker,0,$pos3)][$key])==1)
-								{
-									$mas=$vCard[substr($marker,0,$pos3)][$key];
-									$vCard[substr($marker,0,$pos3)][$key]=array('1'=>$value);
-									$vCard[substr($marker,0,$pos3)][$key][]=$mas;
-								}
-								else{
-									$vCard[substr($marker,0,$pos3)][$key][]=$value;
-								}
-							}
-							else
-							{
-								$vCard[substr($marker,0,$pos3)][$key]=$value;
-							}
-
-							
-				   		}	
-				   		else{
-				   			$perms= $vCard[substr($marker,0,$pos3)];
-				   			$vCard[substr($marker,0,$pos3)][$key]=$value;		   			
-							$vCard[substr($marker,0,$pos3)][]=$perms;
-				   		
-						}
-				   		
-				   }
-  	
-			    }
-		    }
-			else
-			{
-				if($newvar!=''){
-					$marker=substr($marker,0, $pos3);
-					$vCard[$marker]=$newvar;
+				if (!is_readable($Path))
+				{
+					throw new Exception('vCard: Path not accessible ('.$Path.')');
 				}
 
+				$this -> Path = $Path;
+				$this -> RawData = file_get_contents($this -> Path);
 			}
-			
-			return;
-}
- 
- /**
-    * 
-    * Functions which clears text from unneccessary symbols
-    * 
-    * @access public
-    * 
-    * @return cleared text string
-    *
-    * @see vCard()
-    * 
-    */
- 
- public function escape(&$text){
-  $text = str_replace('\:', ':', $text);
-  $text = str_replace('\;', ';', $text);
-  $text = str_replace(';;', ';', $text);
-  $text = str_replace('\,', ',', $text);
-  $text = str_replace("\n", "", $text);
-  return $text;
- }
- 
-  /**
-    * 
-    * Functions which escape text from last symbols
-    *
-    * @access public
-    * 
-    * @return string with text
-    * 
-    */
- public function finalescape(&$text){
- 	 $text = str_replace(";", "", $text);
- 	 $text = str_replace(";;", "", $text);
-	 return $text;
- }
- 
- /**
-    * 
-    * Functions which splits name and address into 
-    * multiple fields
-    *
-    * @access public
-    * 
-    * @return array with values
-    * 
-    */
- 
- 
- public function split($marker,$line_end)
- {	
-	$pos=strpos($marker,';');
-	if($pos!==false){
-		$marker2=substr($marker,0, $pos);
+			elseif ($RawData)
+			{
+				$this -> RawData = $RawData;
+			}
+			else
+			{
+				throw new Exception('vCard: No content provided');
+			}
+
+			// Counting the begin/end separators. If there aren't any or the count doesn't match, there is a problem with the file.
+			// If there is only one, this is a single vCard, if more, multiple vCards are combined.
+			$vCardBeginCount = substr_count($this -> RawData, 'BEGIN:VCARD');
+			$vCardEndCount = substr_count($this -> RawData, 'END:VCARD');
+
+			if (($vCardBeginCount != $vCardEndCount) || !$vCardBeginCount)
+			{
+				$this -> Mode = vCard::MODE_ERROR;
+				throw new Exception('vCard: invalid vCard');
+			}
+
+			$this -> Mode = $vCardBeginCount == 1 ? vCard::MODE_SINGLE : vCard::MODE_MULTIPLE;
+
+			// Removing/changing inappropriate newlines, i.e., all CRs or multiple newlines are changed to a single newline
+			$this -> RawData = str_replace("\r", "\n", $this -> RawData);
+			$this -> RawData = preg_replace('{(\n+)}', "\n", $this -> RawData);
+
+			// In multiple card mode the raw text is split at card beginning markers and each
+			//	fragment is parsed in a separate vCard object.
+			if ($this -> Mode == self::MODE_MULTIPLE)
+			{
+				$this -> RawData = explode('BEGIN:VCARD', $this -> RawData);
+				$this -> RawData = array_filter($this -> RawData);
+
+				foreach ($this -> RawData as $SinglevCardRawData)
+				{
+					// Prepending "BEGIN:VCARD" to the raw string because we exploded on that one.
+					// If there won't be the BEGIN marker in the new object, it will fail.
+
+					$SinglevCardRawData = 'BEGIN:VCARD'."\n".$SinglevCardRawData;
+					$this -> Data[] = new vCard(false, $SinglevCardRawData);
+				}
+			}
+			else
+			{
+				// Joining multiple lines that are split with a hard wrap and indicated by an equals sign at the end of line
+				$this -> RawData = str_replace("=\n", '', $this -> RawData);
+
+				// Joining multiple lines that are split with a soft wrap (space or tab on the beginning of the next line
+				$this -> RawData = str_replace(array("\n ", "\n\t"), '', $this -> RawData);
+
+				$Lines = explode("\n", $this -> RawData);
+
+				foreach ($Lines as $Line)
+				{
+					// Lines without colons are skipped because, most likely, they contain no data.
+					if (strpos($Line, ':') === false)
+					{
+						continue;
+					}
+
+					// Each line is split into two parts. The key contains the element name and additional parameters, if present,
+					//	value is just the value
+					list($Key, $Value) = explode(':', $Line, 2);
+
+					// Key is transformed to lowercase because, even though the element and parameter names are written in uppercase,
+					//	it is quite possible that they will be in lower- or mixed case.
+					$Key = strtolower(trim(self::Unescape($Key)));
+
+					// These two lines can be skipped as they aren't necessary at all.
+					if ($Key == 'begin' || $Key == 'end')
+					{
+						continue;
+					}
+
+					$Value = trim(self::Unescape($Value));
+					$Type = array();
+
+					// Here additional parameters are parsed
+					$KeyParts = explode(';', $Key);
+					$Key = $KeyParts[0];
+
+					if (count($KeyParts) > 1)
+					{
+						// Parameters are split into (key, value) pairs
+						$Parameters = array_map(function($Item)
+						{
+							return explode('=', strtolower($Item));
+						},
+						array_slice($KeyParts, 1));
+
+						// And each parameter is checked whether anything can/should be done because of it
+						foreach ($Parameters as $Parameter)
+						{
+							if (count($Parameter) != 2)
+							{
+								continue;
+							}
+
+							if ($Parameter[0] == 'encoding')
+							{
+								if ($Parameter[1] == 'quoted-printable')
+								{
+									$Value = quoted_printable_decode($Value);
+								}
+							}
+							elseif ($Parameter[0] == 'charset')
+							{
+								if ($Parameter[1] != 'utf-8' && $Parameter[1] != 'utf8')
+								{
+									$Value = mb_convert_encoding($Value, 'UTF-8', $Parameter[1]);
+								}
+							}
+							elseif ($Parameter[0] == 'type')
+							{
+								$Type = explode(',', $Parameter[1]);
+							}
+						}
+					}
+
+					// Values are parsed according to their type
+					if (isset(self::$Spec_StructuredElements[$Key]))
+					{
+						$Value = self::ParseStructuredValue($Value, $Key);
+						if ($Type)
+						{
+							$Value['Type'] = $Type;
+						}
+					}
+					elseif (in_array($Key, self::$Spec_MultipleValueElements))
+					{
+						$Value = self::ParseMultipleTextValue($Value, $Key);
+
+						if ($Type)
+						{
+							$Value = array(
+								'Value' => $Value,
+								'Type' => $Type
+							);
+						}
+					}
+					else
+					{
+						if ($Type)
+						{
+							$Value = array(
+								'Value' => $Value,
+								'Type' => $Type
+							);
+						}
+					}
+
+					if (!isset($this -> Data[$Key]))
+					{
+						$this -> Data[$Key] = array();
+					}
+
+					$this -> Data[$Key][] = $Value;
+				}
+			}
+		}
+
+		/**
+		 * Magic method to get the various vCard values as object members, e.g.
+		 *	a call to $vCard -> N gets the "N" value
+		 *
+		 * @param string Key
+		 *
+		 * @return mixed Value
+		 */
+		public function __get($Key)
+		{
+			if (isset($this -> Data[$Key]))
+			{
+				return $this -> Data[$Key];
+			}
+			elseif ($Key == 'Mode')
+			{
+				return $this -> Mode;
+			}
+			return null;
+		}
+
+		// !Helper methods
+
+	 	/**
+		 * Removes the escaping slashes from the text.
+		 *
+		 * @access private
+		 *
+		 * @param string Text to prepare.
+		 *
+		 * @return string Resulting text.
+		 */
+		private static function Unescape($Text)
+		{
+			return str_replace(array('\:', '\;', ';', '\,', "\n"), array(':', ';', ';', ',', ''), $Text);
+		}
+
+		/**
+		 * Separates the various parts of a structured value according to the spec.
+		 *
+		 * @access private
+		 *
+		 * @param string Raw text string
+		 * @param string Key (e.g., N, ADR, ORG, etc.)
+		 *
+		 * @return array Parts in an associative array.
+		 */
+		private static function ParseStructuredValue($Text, $Key)
+		{
+			$Text = array_map('trim', explode(';', $Text));
+
+			$Result = array();
+			for ($i = 0; $i < count($Text) && $i < count(self::$Spec_StructuredElements[$Key]); $i++)
+			{
+				$Result[self::$Spec_StructuredElements[$Key][$i]] = $Text[$i];
+			}
+			return $Result;
+		}
+
+		/**
+		 * @access private
+		 */
+		private static function ParseMultipleTextValue($Text)
+		{
+			return explode(',', $Text);
+		}
+
+		// !Interface methods
+
+		// Countable interface
+		public function count()
+		{
+			return count($this -> Data);
+		}
+
+		// Iterator interface
+		public function rewind()
+		{
+			reset($this -> Data);
+		}
+
+		public function current()
+		{
+			return current($this -> Data);
+		}
+
+		public function next()
+		{
+			return next($this -> Data);
+		}
+
+		public function valid()
+		{
+			return ($this -> current() !== false);
+		}
+
+		public function key()
+		{
+			return key($this -> Data);
+		}
 	}
-	else{
-		$marker2=$marker;
-	}
-	
-	switch ($marker2) {
-    case 'N'://N;CHARSET=UTF-8;ENCODING=QUOTED-PRINTABLE:Dombrovskis;J=C4=81nis;;;
-		$exploded = explode (';',$marker);
-		$exploded2 = explode (';',$line_end);
-		$exploded2= array_filter($exploded2);
-		if (in_array('ENCODING=QUOTED-PRINTABLE',$exploded)){
-			$returnam = array('LastName'=>trim(quoted_printable_decode($exploded2[0])),'Name'=>trim(quoted_printable_decode($exploded2[1])));
-		}
-		else{
-			$returnam = array('LastName'=>trim($exploded2[0]),'Name'=>trim($exploded2[1]));
-		}
-		return $returnam;
-        break;
-    case 'ADR'://ADR;TYPE=HOME:;;Eksporta 12-256;Riga;;;Latvia
-
-
-		$line_end =substr($line_end,1,strlen($line_end));
-		$line_end = str_replace(';', ' ', $line_end);
-		$exploded2= explode(';',$marker);
-		if (in_array('ENCODING=QUOTED-PRINTABLE',$exploded2)){
-			$returnam = trim(quoted_printable_decode($line_end));
-		}
-		else{
-			$returnam = trim($line_end);
-		}
-
-		return $returnam;  
-		break;
-	case 'URL':
-		$types=substr($this->marker0,9,trim(strlen($this->marker0)));
-		$types=explode(',',$types);
-		foreach($types as $ty)
-		{
-			$returnam[$ty]=trim($line_end);
-		}
-		return $returnam;
-	case 'TEL'://TEL;TYPE=WORK:+371 29594419
-		$types=substr($this->marker0,9,trim(strlen($this->marker0)));
-		$types=explode(',',$types);
-		foreach($types as $ty)
-		{
-			$returnam[$ty]=trim($line_end);
-		}
-		
-		return $returnam;
-		break;
-	case 'EMAIL'://EMAIL;TYPE=WORK,INTERNET:martins@sales.lv
-	    $types=substr($this->marker0,11,trim(strlen($this->marker0)));
-		$types=explode(',',$types);
-		foreach($types as $ty)
-		{
-			$returnam[$ty]=trim($line_end);
-		}
-
-		return $returnam;
-	    break;
-    case 'X-TWITTER':
-	    $types=substr($this->marker0,15,trim(strlen($this->marker0)));
-		$types=explode(',',$types);
-		foreach($types as $ty)
-		{
-			$returnam[$ty]=trim($line_end);
-		}
-		return $returnam;
-    	break;
-    case 'X-SKYPE':
-	    $types=substr($this->marker0,13,trim(strlen($this->marker0)));
-		$types=explode(',',$types);
-		foreach($types as $ty)
-		{
-			$returnam[$ty]=trim($line_end);
-		}
-		return $returnam;
-    	break;
-    case 'FN':
-	    $exploded = explode (';',$marker);
-		$exploded2 = explode (';',$line_end);
-		$exploded2= array_filter($exploded2);
-		if (in_array('ENCODING=QUOTED-PRINTABLE',$exploded)){
-			$returnam = trim(quoted_printable_decode($exploded2[0]));
-		}
-		else{
-		
-			$returnam = trim($exploded2[0]);
-		}
-
-		return $returnam;
-    	break;
-	case 'PHOTO':
-		return '';
-    	break;
-    case 'NOTE':
-    	return '';
-    	break;
-    default:
-		break;
-	}
-
-	return ;
- }
- 
-}
-
 ?>
--- test.php
+++ test.php
@@ -1,8 +1,33 @@
 <?php
-   
-require_once 'parser_class.php';    
-$path = 'contacts-sales-lv-people.vcf';     
-$parse = new vCard($path);
-print_r($parse);
+	require_once('parser_class.php');
 
+	$vCard = new vCard('test2.vcf');
+
+	if (count($vCard) == 0)
+	{
+		throw new Exception('vCard test: empty vCard!');
+	}
+	// if the file contains a single vCard, it is accessible directly.
+	elseif (count($vCard) == 1)
+	{
+		echo '<pre>';
+		print_r($vCard -> N);
+		print_r($vCard -> EMAIL);
+		echo '</pre>';
+	}
+	// if the file contains multiple vCards, they are accessible as elements of an array
+	else
+	{
+		foreach ($vCard as $Index => $vCardPart)
+		{
+			echo '<pre>';
+			print_r($vCardPart -> n);
+			print_r($vCardPart -> tel);
+			print_r($vCardPart -> email);
+			print_r($vCardPart -> phone);
+			print_r($vCardPart -> adr);
+			echo '</pre>';
+			echo '<hr />';
+		}
+	}
 ?>
